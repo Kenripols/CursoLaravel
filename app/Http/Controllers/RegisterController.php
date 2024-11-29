43432081826973dde9b02e57a828dd3602bafb56
Christian CL. Lessa <clessa@DICYT2023.local>
2024-11-29 08:53:06 -0300

    Primer commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register()
+    {
+        return view('register', [
+            'usuario' => 'Dueño1'
+        ]);
+    }
+}
