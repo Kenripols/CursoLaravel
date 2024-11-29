681d9a3edfdf4ba214e51695c0470a140087ae0f
Christian CL. Lessa <clessa@DICYT2023.local>
2024-11-29 10:01:14 -0300

    05 Componentes Blade

---
+++ resources/views/components/alert2.blade.php
@@ -0,0 +1,4 @@
+<div class="p-4 mb-4 text-sm {{$class}} rounded-lg " role="alert">
+    <!-- Defino dos variables para el contenido de la alerta -->
+    <span class="font-medium"> {{$titulo}} </span>  {{$slot}}
+  </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+    <!-- Invoco tailwind css -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Bienvenido a la pagina de registro {{$usuario}} </h1> 
+    <div class="max-w-4x1 mx-auto px-4">
+        <h1>Bienvenido a la pagina de registro {{$usuario}} </h1> 
+    </div>
+    <!-- Alerta usando tailwind css -->
+    <!-- Accediendo a al componente mediante el archivo views/components/alert.blade.php -->
+   <x-alert type="papas">
+    <x-slot name="titulo">Titulo</x-slot>
+     Contenido de la alerta</x-alert>
+    
     
 </body>
 </html>
